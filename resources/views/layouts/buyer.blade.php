876393528fd9bdeea140aec49800e234caf3aebf
ameneses10 <adrianmeneses@hotmail.com>
2020-11-15 21:45:18 +0100

    Add Google Analytics

--- resources/views/layouts/buyer.blade.php
+++ resources/views/layouts/buyer.blade.php
@@ -11,6 +11,16 @@
 
   gtag('config', 'G-84YZNZXFXF');
 </script>
+	
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-177851318-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-177851318-1');
+</script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
